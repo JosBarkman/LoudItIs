ae927c4a20d74b3e4f319642881f678ef5146e1d
Segarraraj <segarraraj@esat-alumni.com>
2022-11-11 11:15:27 +0100

    Fix spectator camera new input system

--- VRInteractions/Assets/FellowshipOfTheFog/Scripts/Player/SpectatorCamera.cs
+++ VRInteractions/Assets/FellowshipOfTheFog/Scripts/Player/SpectatorCamera.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SpectatorCamera : MonoBehaviour
 {
+
     #region Properties
 
     [Header("Settings")]
@@ -14,18 +16,36 @@ public class SpectatorCamera : MonoBehaviour
     public float horizontalRotation = 20.0f;
     public float verticalRotation = 20.0f;
 
+    private PlayerInput input;
+
     #endregion
 
     #region Unity Events
 
+    private void OnEnable()
+    {
+        if (input == null)
+        {
+            input = new PlayerInput();
+        }
+
+        input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
     private void Update()
     {
-        Vector3 translate = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Height"), Input.GetAxis("Vertical")).normalized * Time.deltaTime;
-        Vector3 rotation = new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0.0f).normalized * Time.deltaTime;
+        Vector3 translate = input.Spectator.Movement.ReadValue<Vector3>() * Time.deltaTime;
+        Vector2 rotation = input.Spectator.Rotation.ReadValue<Vector2>() * Time.deltaTime;
 
-        transform.Translate(new Vector3(translate.x * horizontalSpeed, translate.y * verticalSpeed, translate.z * forwardSpeed) );
-        transform.Rotate(new Vector3(0.0f, rotation.y * horizontalRotation, 0.0f), Space.World);
-        transform.Rotate(new Vector3(rotation.x * verticalRotation, 0.0f, 0.0f), Space.Self);
+        transform.Translate(new Vector3(translate.x * horizontalSpeed, translate.y * verticalSpeed, translate.z * forwardSpeed));
+
+        transform.Rotate(new Vector3(0.0f, rotation.x * horizontalRotation, 0.0f), Space.World);
+        transform.Rotate(new Vector3(rotation.y * verticalRotation, 0.0f, 0.0f), Space.Self);
     }
 
     #endregion
