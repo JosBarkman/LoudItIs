a3be1b75b2804ab753daf4d51a4a5ef3d60d7b08
Jozz <jos.barkman@hotmail.com>
2022-12-14 16:53:50 +0100

    HandCollision_Version2
    Upgraded version 1, but still glitches some.
    Added version 2 in which hand detects through preset edge positions such as fingertips, and uses raycast from there to desired position to detect obstacles and pause itself when it does. Does not correctly used signed angle, so will rotate weirdly.

--- VRInteractions/Assets/FellowshipOfTheFog/Scripts/Player/RbFollowTarget.cs
+++ VRInteractions/Assets/FellowshipOfTheFog/Scripts/Player/RbFollowTarget.cs
@@ -3,11 +3,27 @@ using UnityEngine;
 
 [RequireComponent( typeof( Rigidbody ) )]
 public class RbFollowTarget : MonoBehaviour {
+    public enum Hand {
+        LEFT,
+        RIGHT,
+        NONE
+    }
+
+
     [SerializeField]
-    private Transform _target;
+    private Transform _target = null;
+
+    [SerializeField]
+    private Hand _hand = Hand.NONE;
+
+    [SerializeField]
+    private LayerMask _mask = 0;
+
+    [SerializeField]
+    private Transform[] _handEdges = new Transform[0];
 
 
-    private Rigidbody _rb;
+    private Rigidbody _rb = null;
 
 
 
@@ -20,16 +36,76 @@ public class RbFollowTarget : MonoBehaviour {
     }
 
 
-    void FixedUpdate() {
+    private void FixedUpdate() {
         if ( null != _target ) {
-            _rb.velocity = ( _target.position - transform.position ) / Time.fixedDeltaTime;
+            //SetApproach();
 
-            Quaternion rotationDiff = _target.rotation * Quaternion.Inverse( transform.rotation );
-            rotationDiff.ToAngleAxis( out float angleDeg, out Vector3 rotationAxis );
-
-            Vector3 rotationDiffDeg = angleDeg * rotationAxis;
-
-            _rb.angularVelocity = rotationDiffDeg * Mathf.Deg2Rad / Time.fixedDeltaTime;
+            VelocityApproach();
         }
+    }
+
+
+    /*private void SetApproach() {
+        bool collided = false;
+        for ( int i = 0; i < _handEdges.Length; ++i ) {
+            Vector3 handEdge = Vector3.RotateTowards( _handEdges[i].localPosition, _target.forward, Mathf.PI * 2f, Mathf.Infinity );
+            Vector3 ourPosition = transform.position + handEdge;
+            Vector3 desiredPosition = _target.position + handEdge;
+
+            Ray ray = new Ray( ourPosition, ( desiredPosition - ourPosition ).normalized );
+            //Vector3 stopPoint = _target.position;
+
+            if ( Physics.Raycast( ray, out RaycastHit hit, ( desiredPosition - ourPosition ).magnitude, _mask, QueryTriggerInteraction.Ignore ) ) {
+                //stopPoint = hit.point;
+                collided = true;
+            }
+            //else {
+            //    transform.position = _target.position;
+            //}
+
+            //transform.position = stopPoint;
+            switch ( _hand ) {
+                case Hand.LEFT:
+                    transform.rotation = _target.rotation * Quaternion.Euler( -90, 90, 0 );
+                    break;
+                case Hand.RIGHT:
+                    transform.rotation = _target.rotation * Quaternion.Euler( 90, -90, 0 );
+                    break;
+                default:
+                    transform.rotation = _target.rotation;
+                    break;
+            }
+        }
+
+        if ( !collided ) {
+            transform.position = _target.position;
+        }
+    }*/
+
+
+    private void VelocityApproach() {
+        Quaternion targetRotation;
+        switch ( _hand ) {
+            case Hand.LEFT:
+                targetRotation = _target.rotation * Quaternion.Euler( -90, 90, 0 );
+                break;
+            case Hand.RIGHT:
+                targetRotation = _target.rotation * Quaternion.Euler( 90, -90, 0 );
+                break;
+            default:
+                targetRotation = _target.rotation;
+                break;
+        }
+
+        _rb.velocity = ( _target.position - transform.position ) / Time.fixedDeltaTime;
+        _rb.MoveRotation( targetRotation );
+
+
+        /*Quaternion rotationDiff = targetRotation * Quaternion.Inverse( transform.rotation );
+        rotationDiff.ToAngleAxis( out float angleDeg, out Vector3 rotationAxis );
+
+        Vector3 rotationDiffDeg = angleDeg * rotationAxis;
+
+        _rb.angularVelocity = rotationDiffDeg * Mathf.Deg2Rad / Time.fixedDeltaTime;*/
     }
 }
