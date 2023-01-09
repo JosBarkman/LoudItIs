5fa69e23cba309316d14a2a85c8c523ac896648a
Segarraraj <segarraraj@esat-alumni.com>
2023-01-09 13:28:01 +0100

    Actor role button disabled when all characters are locked

--- VRInteractions/Assets/FellowshipOfTheFog/Scripts/UI/Menu Controllers/MenuControllerRoleSelector.cs
+++ VRInteractions/Assets/FellowshipOfTheFog/Scripts/UI/Menu Controllers/MenuControllerRoleSelector.cs
@@ -12,6 +12,9 @@ public class MenuControllerRoleSelector : MonoBehaviour
     [SerializeField]
     private Button spectatorRoleButton;
 
+    [SerializeField]
+    private Button actorRoleButton;
+
     [Header("External controllers")]
     [SerializeField]
     private MenuControllerCharacterSelector characterSelectorController;
@@ -55,6 +58,19 @@ public class MenuControllerRoleSelector : MonoBehaviour
         {
             roleSelectionController.PickRoleAndCharacter(Role.Spectator, null);
         });
+
+        bool characterAvailable = false;
+        var enumerator = roleSelectionController.lockedCharacters.GetEnumerator();
+
+        while (characterAvailable == false && enumerator.MoveNext())
+        {
+            characterAvailable = !enumerator.Current.Value;
+        }
+
+        if (!characterAvailable)
+        {
+            actorRoleButton.interactable = false;
+        }
     }   
 
     #endregion
